82c2636cef38796a5bf7da1abe65e3ed50e6a011
Alessio Bolognino <themolok@gmail.com>
2011-10-19 19:12:11 +0200

    Revert "Derp"
    This reverts commit 12ce0d42b308caa7929e87b42aaf829c7631377f.

--- elicit/elicit.py
+++ elicit/elicit.py
@@ -30,10 +30,8 @@ except ImportError:
   raise SystemExit
 
 # TODO:
-# [X] NameThatColor
-# [/] Terminal name
-# [ ] remove dead code
-# [ ] implement a Color that doesn't emit anything (to replace FakeColor)
+# [/] NameThatColor
+# [ ] Terminal name
 # [ ] Refresh after zoom in/out
 # [ ] tab order
 # [ ] it's hard to use right click with a touchpad with
@@ -347,11 +345,8 @@ class Elicit:
     self.notebook.append_page(frame_wheel, frame_wheel_label)
 
     # TODO: do not hardcode tha palette path!
-#
-    self.name_palette = NamePalette('/home/alebolo/Projects/elicit/elicit-molok-github/ntc.txt',
-            self.color)
     #self.name_palette = NamePalette('../ntc.txt', self.color)
-    #self.name_palette = NamePalette('/etc/X11/rgb.txt', self.color)
+    self.name_palette = NamePalette('/etc/X11/rgb.txt', self.color)
     self.name_treeview = ColorName(self.name_palette)
     #self.h_ids['name'] = self.name_combobox.connect('changed', self.name_combobox_changed)
     #name_hbox.pack_start(self.name_combobox, True, True, 2)
